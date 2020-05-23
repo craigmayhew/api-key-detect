b37d8ebb90c550fed6bf74f245877a1e9f23eb31
Craig Mayhew <craig@mayhew.io>
2020-05-23 20:43:01 +0100

    ignored list now alphabetical

--- api_key_detect.py
+++ api_key_detect.py
@@ -1,6 +1,6 @@
 import sys, os, re, itertools
 
-ignored = ['.git', 'node_modules', 'bower_components', '.sass-cache', '.png', '.ico', '.mov']
+ignored = ['.git', '.ico', '.mov', '.png', '.sass-cache', 'bower_components', 'node_modules']
 api_key_min_entropy_ratio = 0.5
 api_key_min_length = 7
 
